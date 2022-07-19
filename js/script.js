c5711547d2e9bc08eff5c3cd33a969d7b46efddc
Markus Ager <agermarkus123@gmail.com>
2022-07-19 23:16:53 +0200

    commented prio out

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 const taskslistContainer = document.querySelector(".tasks")
 
-
 const printTasks = () => {
-    if (localStorage.getItem("tasks") == null) return;
-    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")));
+    if (localStorage.getItem("tasks") == null) return
+    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")))
 
-    taskslistContainer.innerHTML = "";
+    taskslistContainer.innerHTML = ""
     tasksList.forEach(task => {
         taskslistContainer.innerHTML += `
         <div class="col mb-4">
@@ -21,13 +20,16 @@ const printTasks = () => {
                     <img src=${task.image} class="card-img-top border p-1" style="object-fit: cover" alt="${task.taskName}">
                     <h2 class="card-title text-center my-3">${task.taskName}</h2>
                     <p class="m-0">${task.description}</p>
-                    
-                    <div class="float-left mb-3 mt-auto">
+                
+                    <hr>
+
+                    <!-- <div class="float-left mb-3 mt-auto">
                         <hr>
                         <i class="bi bi-exclamation-triangle-fill"></i>
                         <span>Priority level: </span>
                         <span class="btn text-white btn-success importance p-1 rounded priorityBtn" role="button">${task.importance}</span>
-                    </div>
+                    </div> -->
+
                     <div class="float-left">
                         <i class="bi bi-calendar3"></i>
                         <span>Deadline: </span>
@@ -48,78 +50,80 @@ const printTasks = () => {
             </div>
         </div>
         `
-    });
-};
-
-const increasePriority = () => {
-    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")));
-
-    const priorityBtn = document.querySelectorAll(".priorityBtn")
-    priorityBtn.forEach((btn, i) => {
-        btn.addEventListener("click", () => {
-            tasksList[i].importance++
-            btn.innerHTML = tasksList[i].importance
-            priorityColor()
-        })
     })
+    // increasePriority()
+    // priorityColor()
 }
 
-const priorityColor = () => {
-    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")));
+// const increasePriority = () => {
+//     let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")))
 
-    const priorityBtn = document.querySelectorAll(".priorityBtn")
-    priorityBtn.forEach((btn, i) => {
-        if (tasksList[i].importance == 0) {
-            btn.classList.remove("btn-danger", "btn-warning")
-            btn.classList.add("btn-success")
-        }
-        else if (tasksList[i].importance <= 2) {
-            btn.classList.remove("btn-danger", "btn-success")
-            btn.classList.add("btn-warning")
-        } else if (tasksList[i].importance >= 3) {
-            btn.classList.remove("btn-warning", "btn-success")
-            btn.classList.add("btn-danger")
-        }
-        if (tasksList[i].importance == 6) {
-            tasksList[i].importance = 0
-            btn.classList.remove("btn-warning", "btn-danger")
-            btn.classList.add("btn-success")
-            btn.innerHTML = tasksList[i].importance
-        }
-    })
-}
+//     const priorityBtn = document.querySelectorAll(".priorityBtn")
+//     priorityBtn.forEach((btn, i) => {
+//         btn.addEventListener("click", () => {
+//             tasksList[i].importance++
+//             btn.innerHTML = tasksList[i].importance
+//             priorityColor()
+//         })
+//     })
+// }
 
-const sortTasks = () => {
-    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")));
+// const priorityColor = () => {
+//     let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")))
 
-    var icon = document.getElementById("sortIcon")
+//     const priorityBtn = document.querySelectorAll(".priorityBtn")
+//     priorityBtn.forEach((btn, i) => {
+//         if (tasksList[i].importance == 0) {
+//             btn.classList.remove("btn-danger", "btn-warning")
+//             btn.classList.add("btn-success")
+//         }
+//         else if (tasksList[i].importance <= 2) {
+//             btn.classList.remove("btn-danger", "btn-success")
+//             btn.classList.add("btn-warning")
+//         } else if (tasksList[i].importance >= 3) {
+//             btn.classList.remove("btn-warning", "btn-success")
+//             btn.classList.add("btn-danger")
+//         }
+//         if (tasksList[i].importance == 6) {
+//             tasksList[i].importance = 0
+//             btn.classList.remove("btn-warning", "btn-danger")
+//             btn.classList.add("btn-success")
+//             btn.innerHTML = tasksList[i].importance
+//         }
+//     })
+// }
 
-    if (icon.className === "bi bi-sort-up") {
-        icon.className = "bi bi-sort-down";
-        tasksList.sort(function (a, b) {
-            return b.importance - a.importance
-        });
-    } else {
-        icon.className = "bi bi-sort-up";
-        tasksList.sort(function (a, b) {
-            return a.importance - b.importance
-        });
-    }
+// const sortTasks = () => {
+//     let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")))
 
-    printTasks()
-    increasePriority()
-    priorityColor()
-}
+//     var icon = document.getElementById("sortIcon")
+
+//     if (icon.className === "bi bi-sort-up") {
+//         icon.className = "bi bi-sort-down"
+//         tasksList.sort(function (a, b) {
+//             return b.importance - a.importance
+//         })
+//     } else {
+//         icon.className = "bi bi-sort-up"
+//         tasksList.sort(function (a, b) {
+//             return a.importance - b.importance
+//         })
+//     }
+
+//     printTasks()
+//     increasePriority()
+//     priorityColor()
+// }
 
 const removeTask = (id) => {
-    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")));
+    let tasksList = Array.from(JSON.parse(localStorage.getItem("tasks")))
 
     console.log(tasksList)
     tasksList.splice(tasksList.indexOf(id), 1)
-    localStorage.setItem("tasks", JSON.stringify(tasksList));
+    localStorage.setItem("tasks", JSON.stringify(tasksList))
     printTasks()
-    increasePriority()
-    priorityColor()
+    // increasePriority()
+    // priorityColor()
 }
 
 const addTask = () => {
@@ -128,26 +132,26 @@ const addTask = () => {
         taskName: document.getElementById("title").value,
         image: document.getElementById("img").value,
         description: document.getElementById("desc").value,
-        importance: document.getElementById("prio").value,
+        // importance: document.getElementById("prio").value,
         deadline: document.getElementById("dl").value
-    }]));
+    }]))
 
     console.log(localStorage)
 
     document.getElementById("title").value = ""
     document.getElementById("desc").value = ""
-    document.getElementById("prio").value = "0"
+    // document.getElementById("prio").value = "0"
     document.getElementById("dl").value = ""
     document.getElementById("img").value = ""
 
     printTasks()
-    increasePriority()
-    priorityColor()
+    // increasePriority()
+    // priorityColor()
 }
 
 document.getElementById("addTaskBtn").addEventListener("click", e => {
-    e.preventDefault();
-    addTask();
-});
+    e.preventDefault()
+    addTask()
+})
 
 printTasks()
